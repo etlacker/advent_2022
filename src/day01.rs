53e3dd8b8dc23e9c1449ff6acce9479eac2b30d0
Eric L <31419044+etlacker@users.noreply.github.com>
2022-12-15 15:24:49 -0500

    Day 1, part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,14 @@
 pub fn part_one(input: &str) -> Option<u32> {
+    let sums = sorter_helper(input);
+    return Some(sums[0]);
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let sums = sorter_helper(input);
+    return Some(vec![sums[0], sums[1], sums[2]].iter().sum());
+}
+
+fn sorter_helper(input: &str) -> Vec<u32> {
     let mut sums = input
         .split("\n\n")
         .map(|x| {
@@ -12,6 +22,5 @@ pub fn part_one(input: &str) -> Option<u32> {
         .collect::<Vec<u32>>();
     sums.sort();
     sums.reverse();
-    println!("highest: {}", sums[0]);
-    return Some(sums[0]);
+    return sums;
 }
--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,15 @@ fn main() {
     let input = read_file("inputs", 01);
 
     let start_time = Instant::now();
-    day01::part_one(&input);
+    println!("Day 1 Part 1: Highest: {:?}", day01::part_one(&input).unwrap());
+    println!("Day 1 Part 2: Sum of Highest 3: {:?}", day01::part_two(&input).unwrap());
     println!("Total time: {:?}", Instant::now() - start_time);
 
 }
 
 fn read_file(folder: &str, day: u8) -> String {
     let cwd = env::current_dir().unwrap();
-
     let filepath = cwd.join("src").join(folder).join(format!("{:02}.txt", day));
-
     let f = fs::read_to_string(filepath);
     f.expect("could not open input file")
 }
