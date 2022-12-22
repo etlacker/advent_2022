a7c16531abbb69e5b6e197b434e4c7d41b9bfd96
Eric L <31419044+etlacker@users.noreply.github.com>
2022-12-21 23:17:01 -0500

    Complete day 2 p 2

--- src/day02.rs
+++ src/day02.rs
@@ -12,7 +12,26 @@ pub fn part_one(input: &str) -> Option<i32> {
             score += 6;
         } // else elf beats me, no points awarded
         score += me + 1;
-        println!("{}: {} - {} --> score: {}", play, elf, me, score);
+    }
+    return Some(score);
+}
+
+pub fn part_two(input: &str) -> Option<i32> {
+    let plays = input
+        .split("\n");
+    let mut score: i32 = 0;
+    for play in plays {
+        let elf: i32 = play.as_bytes()[0] as i32 - 65;
+        let outcome: i32 = play.as_bytes()[2] as i32 - 88;
+        if outcome == 0 { // Outcome = X = 0 = LOSE
+            score += ((elf + 2) % 3) + 1;
+        }
+        else if outcome == 2 { // Outcome = Z = 2 = WIN
+            score += ((elf + 1) % 3) + 1;
+        } else { // Outcome = Y = 1 = TIE
+            score += elf + 1;
+        }
+        score += outcome * 3; // account for lose (0), tie (3), win (6)
     }
     return Some(score);
 }
--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     let start_time = Instant::now();
     // println!("Day 1 Part 1: Highest: {:?}", day01::part_one(&input).unwrap());
     // println!("Day 1 Part 2: Sum of Highest 3: {:?}", day01::part_two(&input).unwrap());
-    println!("Day 2 Part 1: Score: {:?}", day02::part_one(&input).unwrap());
+    println!("Day 2 Part 1: Score: {:?}", day02::part_two(&input).unwrap());
     println!("Total time: {:?}", Instant::now() - start_time);
 
 }
